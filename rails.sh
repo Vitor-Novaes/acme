229dcd4b461e848e489217c3afeefd102f1cc40c
Vitor-Novaes <vitornovaes27@gmail.com>
2022-06-12 05:33:26 -0300

    style(rubocop) style configs editor

---
+++ rails.sh
@@ -0,0 +1,12 @@
+kill -9 `cat tmp/pids/server.pid`
+echo ""
+echo "   .     '     ,"
+echo "     _________"
+echo "  _ /_|_____|_\ _"
+echo "    '. \   / .'"
+echo "      '.\ /.'"
+echo "        '.'"
+echo " "
+echo ""
+echo ""
+rails s
